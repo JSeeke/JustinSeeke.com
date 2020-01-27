67960de47b24c13d735f9691883febf589e558c2
Justin Seeke <jjseeke@gmail.com>
2020-01-26 18:52:55 -0600

    Initial QR web app commit

--- WebDesign/javascript/site.js
+++ WebDesign/javascript/site.js
@@ -1,24 +1,11 @@
 function scrollToSection(section) {
-	var height = $(window).height();
-	switch (section) {
-		case "aboutMe":
-			for (i=0; i<height; i++) {
-				window.scrollTo(0, i);
-			}
-			break;
-		case "myStory":
-			height *= 2;
-			for (i=0; i<height; i++) {
-				window.scrollTo(0, i);
-				setTimeout(function() {}, 10);
-			}
-			break;
-		case "myProjects":
-			height *= 3;
-			for (i=0; i<height; i++) {
-				window.scrollTo(0, i);
-				setTimeout(function() {}, 10);
-			}
-			break;
-	}
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+  )
 }
